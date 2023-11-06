db28a5855fac6afaf955c21024070486eb29d7a6
realmilkmiuku <60050214@kmitl.ac.th>
2023-11-06 16:36:55 +0700

    Create ui and fragment folder - update MainActivity to NewsActivity - move NewsActivity to ui folder

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -1,10 +1,12 @@
-package com.androiddevs.mvvmnewsapp
+package com.androiddevs.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.androiddevs.mvvmnewsapp.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
 
+    //Hi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
