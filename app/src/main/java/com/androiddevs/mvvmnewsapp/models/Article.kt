dd3a54c0a18ccd195d192591640ab0adaa582fd8
realmilkmiuku <60050214@kmitl.ac.th>
2023-11-07 22:21:14 +0700

    fixed saved button doesn't work because non-null check

--- app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
@@ -4,15 +4,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
 
-
 @Entity(
     tableName = "articles"
 )
-
 data class Article(
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null,
-
     val author: String,
     val content: String,
     val description: String,
