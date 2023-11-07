b46ef16f7c7b7743b1ac56a6d681d4564d950351
realmilkmiuku <60050214@kmitl.ac.th>
2023-11-07 16:58:27 +0700

    Step2 -> Step13

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "5233b65f8ab54a1b9893cb21b4737929"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
